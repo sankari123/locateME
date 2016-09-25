7518a824b3360ceea937813b20f7ae7395f2442b
sankari123 <write2sankari@yahoo.com>
2016-09-25 17:52:51 +0530

    Update tstphp.php

--- tstphp.php
+++ tstphp.php
@@ -11,17 +11,17 @@ $server = 'AndroidDb1.db.5014787.hostedresource.com';
 
 mysql_connect("AndroidDb1.db.5014787.hostedresource.com", "AndroidDb1", "Peace4all#") or die(mysql_error()); 
 mysql_select_db("AndroidDb1") or die(mysql_error());  
-
+print $_GET["lat1"];
  // this is the workaround for file_get_contents(...)
-  echo $_POST["lat1"];
-  if(isset($_POST['lat1']) && $_POST['long1']!='')
+  echo $_GET["lat1"];
+  if(isset($_GET['lat1']) && $_GET['long1']!='')
   {
 $lat1=trim($_GET["lat1"]);
 $long1=trim($_GET["long1"]);
 $csv_output='';
 $sSQL= "SELECT loc_id, addr1,addr2,addr3,addr4,addr5,phone,latitude,longitude, ( 6371 * acos( cos( radians($lat1) ) * cos( radians( DIVE_LOC.latitude ) ) * cos( radians(DIVE_LOC.longitude) - radians($long1)) + sin(radians($lat1))  * sin( radians(DIVE_LOC.latitude)))) AS distance FROM DIVE_LOC HAVING distance < 30 ORDER BY distance LIMIT 0 , 11";
 //$sSQL= "SELECT loc_id, addr1 from DIVE_LOC";
-//echo $sSQL;
+echo $sSQL;
 $result = mysql_query($sSQL);
 //$numberOfRows = mysql_num_rows($result);
 $numberOfRows = mysql_num_rows($result);
